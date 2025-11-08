b74a8c96a684fbef48120486f722b35cf87e1902
Sam_76 <muhammadusamah76@gmail.com>
2025-11-09 04:13:09 +0800

    adding cors & bodyparser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
