1808e55087834a65552b57ee8b87b9094e7f8bc8
Sam_76 <muhammadusamah76@gmail.com>
2026-01-05 23:05:12 +0800

    add react-router for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Name : YupYup</h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/Login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
