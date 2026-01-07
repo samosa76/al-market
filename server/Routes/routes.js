7043e15a894867b0f5d85fe4455216d3fb2c5eb5
Sam_76 <muhammadusamah76@gmail.com>
2026-01-08 05:22:21 +0800

    make a handler for RFID

--- server/Routes/routes.js
+++ server/Routes/routes.js
@@ -3,6 +3,11 @@ const routes = express.Router();
 const db = require('../database/database');
 const response = require("../response/response");
 
+/**
+ * Ini import hasil dari serial port
+ */
+const rfuid = require('../Handler/UidHandler');
+
 
 routes.get('/', (req, res) => {
     res.send("Hellow");
@@ -26,6 +31,22 @@ routes.get('/api_transaction', (req, res) => {
     db.query(sql, (err, result) => {
         response(res, 200, result, "GET ALL TRANSAKSI");
     })
+})
+
+
+/**
+ * 
+ * Coba buka http://localhost:8000/api_uid
+ * kalo di bagian data : "(Uid)" ini udah masuk sesuai dengan uid card berarti success
+ * 
+ */
+routes.get('/api_uid', (req,res) => {
+    /**
+     * 
+     * Disini masukin rfuid yang tadi jadi json
+     * 
+     */
+    response(res, 200, rfuid, "GET UID");
 })
 
 module.exports = routes;
