33bd04eaa4233b9bfc518921cd7cf720193ad006
unknown <muhammadmiftahusshiddiq@gmail.com>
2026-01-08 16:07:34 +0700

    buyerpage

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,7 +1,70 @@
+import style from "./Home.module.css"
+
 function Home() {
-    return(
-        <div>
-            <h1>Home Page</h1>
+    return (
+
+        <div className={style.container}>
+            <div className={style.container1}>
+
+                <div className={style.topup}>
+
+                    <div className={style.nama_kelas}>
+
+                        <div className={style.identitas}>
+                            <p>Nama : </p>
+                            <p>Kelas : </p>
+                        </div>
+                        <div className={style.topup_button}>
+                            <img src="/image/arrow.png" alt="arrow" className={style.arrow}></img>
+                            <div className={style.income}>
+                                <p style={{ fontSize: "8px", color: "white" }}>Income</p>
+                                <h4 style={{ color: "white" }}>Rp.000.000.000</h4>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className={style.balance}>
+                        <div className={style.total_saldo}>
+                            <p>Total Balance</p>
+                            <p className={style.saldo}>Rp.000.000.000</p>
+                        </div>
+                        <div className={style.exepense_button}>
+                            <img src="/image/arrow.png" alt="arrow" className={style.arrow}></img>
+                            <div className={style.income}>
+                                <p style={{ fontSize: "8px", color: "white" }}>Income</p>
+                                <h4 style={{ color: "white" }}>Rp.000.000.000</h4>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className={style.grafik}>
+
+                </div>
+
+            </div>
+
+            <div className={style.container2}>
+
+                <div className={style.date}>
+
+                </div>
+
+                <div className={style.history}>
+                    <div className={style.title}>
+                        <p>NO</p>
+                        <p className={style.title_name}>Nama Barang</p>
+                        <p>ID Barang</p>
+                        <p>Total Belanja</p>
+                        <p>Waktu</p>
+                    </div>
+                    <hr />
+                </div>
+
+            </div>
+
         </div>
     )
 }
