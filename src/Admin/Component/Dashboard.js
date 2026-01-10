27afc95126bf4e43cfa608cc193b98cbe52d18d5
Sam_76 <muhammadusamah76@gmail.com>
2026-01-10 19:27:20 +0800

    Polishing Admin Dashboard

--- src/Admin/Component/Dashboard.js
+++ src/Admin/Component/Dashboard.js
@@ -1,11 +1,31 @@
 import style from './Dashboard.module.css'
 
 function Dashboard() {
-    return(
+    return (
         <div className={`${style.container} ${style.flex_center}`}>
-            <div className={style.container_menu}></div>
-            <div className={style.container_menu}></div>
-            <div className={style.container_menu}></div>
+            <div className={`${style.container_menu} ${style.card}`}>
+                <Menu title="Masukan Barang" />
+            </div>
+            <div className={`${style.container_menu} ${style.card}`}>
+                <Menu title="Tambah Penjual" />
+            </div>
+            <div className={`${style.container_menu} ${style.card}`}>
+                <Menu title="Tambah Pembeli" />
+            </div>
+        </div>
+    )
+}
+
+function Menu({ title }) {
+    return (
+        <div className={style.menu}>
+
+            <img src="/image/Admin/image_barang_admin.png" alt="Logo" className={style.menu_logo} />
+
+            <div className={style.menu_desc}>
+                <p>{title}</p>
+            </div>
+
         </div>
     )
 }
